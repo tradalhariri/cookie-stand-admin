19ee0991e748866ea13821d9af30947a947d6df3
tradalhariri <trad.alhariri@gmail.com>
2021-12-26 21:00:13 +0200

    add url to readme file

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -12,7 +12,7 @@ const LoginForm = () => {
     login(username,password);
   };
     return (
-        <div className="flex flex-col items-center justify-center flex-1 w-full text-center bg-green-50 my-20">
+        <div className="flex flex-col items-center justify-center flex-1 w-full my-20 text-center bg-green-50">
         <div className="flex w-2/3 bg-green-200 rounded-lg">
             <form className = "w-full px-10 py-5 border-4 border-green-500 border-solid rounded-xl" onSubmit = {hanldeLogin}>
                 <div className="w-full px-3 ">
@@ -23,7 +23,7 @@ const LoginForm = () => {
                 </div>
 
                 <div className="w-full px-3 mt-5">
-                    <label className="block w-full my-2 mt-5 text-xl font-bold tracking-wide text-gray-800" for="userName">
+                    <label className="block w-full my-2 mt-5 text-xl font-bold tracking-wide text-gray-800" for="password">
                         PASSWORD
                     </label>
                     <input className="block w-full px-4 py-4 leading-tight text-gray-700 bg-gray-100 border border-gray-200 appearance-none focus:outline-none focus:bg-white focus:border-gray-200" name="password" id="password" type="password" placeholder = "password" required onChange={(e) => setPassword(e.target.value)} />
